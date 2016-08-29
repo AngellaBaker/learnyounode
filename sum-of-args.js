2ee8e391578a0fd19261b6a3f6cfd0369735d1d4
AngellaBaker <angellambaker@gmail.com>
2016-08-29 15:59:25 -0500

    adding challenge 2 of learnyounode -baby steps

--- sum-of-args.js
+++ sum-of-args.js
@@ -1,5 +1,9 @@
+// this program is to go through a list of console supplied arguments and return a sum of numbers.
+// define sum
 var sum = 0
+// loop, excludes node and program location, adds values to sum
 for (i = 2; i < process.argv.length; i++) {
   sum += +process.argv[i];
 }
+// prints sum to console
 console.log(sum);
